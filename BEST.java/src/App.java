408d7db97c70779645768232613c04b82999a312
22000023 <m66tshilidzi@gmail.com>
2023-09-07 12:23:01 +0200

    done

--- BEST.java/src/App.java
+++ BEST.java/src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        /*var expectedNames = ["Andani", "andani"];
+                var expectedLastNames = ["Mulaudzi", "mulaudzi"];
+                var expectedUserNames = ["CupCake", "cupcake","Cupcake","Babe","Baby","babe","baby"];
+                var expectedPasswords = ["I love you", "i love u", "I Love You", "I LOVE YOU", "I lOvE YoU"]; */
     }
 }
